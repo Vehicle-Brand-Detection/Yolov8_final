d420b700c35c172fba2f9a8cc3834716b1a8c22e
Anjali2201 <anjalikushwaha031@gmail.com>
2023-09-03 17:33:54 +0530

    Folder Structure

---
+++ app.py
@@ -0,0 +1,88 @@
+from pathlib import Path
+import streamlit as st
+import config
+from utils import load_model, infer_uploaded_image, infer_uploaded_video, infer_uploaded_webcam, show_chart
+
+
+# setting page layout
+st.set_page_config(
+    page_title="Vehicle Brand Recognition",
+    page_icon="🤖",
+    layout="wide",
+    initial_sidebar_state="expanded"
+    )
+
+# main page heading
+st.title("Vehicle Brand Recognition")
+
+# sidebar
+st.sidebar.header("Configurations")
+
+# model options on sidebar
+task_type = st.sidebar.selectbox(
+    "Select Task",
+    ["Detection"]
+)
+
+model_type = None
+if task_type == "Detection":
+    model_type = st.sidebar.selectbox(
+        "Select Model",
+        config.DETECTION_MODEL_LIST
+    )
+else:
+    st.error("Currently only 'Detection' function is implemented")
+
+confidence = float(st.sidebar.slider(
+    "Select Model Confidence", 30, 100, 50)) / 100
+
+model_path = ""
+if model_type:
+    model_path = Path(config.DETECTION_MODEL_DIR, str(model_type))
+else:
+    st.error("Please Select Model in Sidebar")
+
+# load pretrained DL model
+try:
+    model = load_model(model_path)
+except Exception as e:
+    st.error(f"Unable to load model. Please check the specified path: {model_path}")
+
+# image/video options
+st.sidebar.header("Image/Video Config")
+source_selectbox = st.sidebar.selectbox(
+    "Select Source",
+    config.SOURCES_LIST
+)
+
+source_img = None
+if source_selectbox == config.SOURCES_LIST[0]: # Image
+    infer_uploaded_image(confidence, model)
+elif source_selectbox == config.SOURCES_LIST[1]: # Video
+    infer_uploaded_video(confidence, model)
+elif source_selectbox == config.SOURCES_LIST[2]: # Webcam
+    infer_uploaded_webcam(confidence, model)
+else:
+    st.error("Currently only 'Image' and 'Video' source are implemented")
+
+
+st.sidebar.header("Charts")
+source_chart = st.sidebar.selectbox(
+    "Select Chart",
+    config.CHARTS
+)
+
+selected_chart = None
+
+if source_chart == config.CHARTS[1]: # F1 Curve
+    show_chart(1)
+elif source_chart == config.CHARTS[2]: # R Curve
+    show_chart(2)
+elif source_chart == config.CHARTS[3]: # P Curve
+    show_chart(3)
+elif source_chart == config.CHARTS[4]: # Confusion Matrix 
+    show_chart(4)
+elif source_chart == config.CHARTS[5]: # PR Curve
+    show_chart(5)
+elif source_chart == config.CHARTS[6]: # Results
+    show_chart(6)
---
+++ utils.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   @File Name:     utils.py
+   @Author:        Luyao.zhang
+   @Date:          2023/5/16
+   @Description:
+-------------------------------------------------
+"""
+from ultralytics import YOLO
+import streamlit as st
+import cv2
+from PIL import Image
+import tempfile
+
+
+def _display_detected_frames(conf, model, st_frame, image):
+    """
+    Display the detected objects on a video frame using the YOLOv8 model.
+    :param conf (float): Confidence threshold for object detection.
+    :param model (YOLOv8): An instance of the `YOLOv8` class containing the YOLOv8 model.
+    :param st_frame (Streamlit object): A Streamlit object to display the detected video.
+    :param image (numpy array): A numpy array representing the video frame.
+    :return: None
+    """
+    # Resize the image to a standard size
+    image = cv2.resize(image, (720, int(720 * (9 / 16))))
+
+    # Predict the objects in the image using YOLOv8 model
+    res = model.predict(image, conf=conf)
+
+    # Plot the detected objects on the video frame
+    res_plotted = res[0].plot()
+    # result=model.track(image,show=True,tracker="bytetrack.yaml")
+    st_frame.image(res_plotted,
+                   caption='Detected Video',
+                   channels="BGR",
+                   use_column_width=True
+                   )
+
+
+@st.cache_resource
+def load_model(model_path):
+    """
+    Loads a YOLO object detection model from the specified model_path.
+
+    Parameters:
+        model_path (str): The path to the YOLO model file.
+
+    Returns:
+        A YOLO object detection model.
+    """
+    model = YOLO('yolov8n-seg.pt')
+    model = YOLO("best.pt")
+    return model
+
+
+def infer_uploaded_image(conf, model):
+    """
+    Execute inference for uploaded image
+    :param conf: Confidence of YOLOv8 model
+    :param model: An instance of the `YOLOv8` class containing the YOLOv8 model.
+    :return: None
+    """
+    source_img = st.sidebar.file_uploader(
+        label="Choose an image...",
+        type=("jpg", "jpeg", "png", 'bmp', 'webp')
+    )
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        if source_img:
+            uploaded_image = Image.open(source_img)
+            # adding the uploaded image to the page with caption
+            st.image(
+                image=source_img,
+                caption="Uploaded Image",
+                use_column_width=True
+            )
+
+    if source_img:
+        if st.button("Execution"):
+            with st.spinner("Running..."):
+                res = model.predict(uploaded_image,
+                                    conf=conf)
+                boxes = res[0].boxes
+                res_plotted = res[0].plot()[:, :, ::-1]
+
+                with col2:
+                    st.image(res_plotted,
+                             caption="Detected Image",
+                             use_column_width=True)
+                    try:
+                        with st.expander("Detection Results"):
+                           # Vehicle Brand List
+                            cntVeh={'Volkswagen':0,'Tata':0,'Hyundai':0,'Honda':0,'Ford':0,'Maruti Suzuki':0,'Renault':0,'Mahindra':0,'BMW':0,'Kia':0,'Mercedes':0,'Audi':0}
+                            for result in res:                                         # iterate results
+                                boxes = result.boxes.cpu().numpy()                         # get boxes on cpu in numpy
+                                for box in boxes:                                          # iterate boxes
+                                        cntVeh[result.names[int(box.cls[0])]]+=1           # counting number of vehicles and thier brands.
+                            # st.write(f"Vehicle Count: {}")
+                            for key,val in cntVeh.items():
+                                if(val>0):
+                                    st.write(f"Vehicle Detected: {key}-->{val}")      # Displaying the results. Company name and thier count.
+
+                            
+                    except Exception as ex:
+                        st.write("No image is uploaded yet!")
+                        st.write(ex)
+
+
+def infer_uploaded_video(conf, model):
+    """
+    Execute inference for uploaded video
+    :param conf: Confidence of YOLOv8 model
+    :param model: An instance of the `YOLOv8` class containing the YOLOv8 model.
+    :return: None
+    """
+    source_video = st.sidebar.file_uploader(
+        label="Choose a video..."
+    )
+
+    # if source_video:
+    #     
+
+    if source_video:
+        st.video(source_video)
+        if st.button("Execution"):
+            with st.spinner("Running..."):
+                try:
+                    tfile = tempfile.NamedTemporaryFile()
+                    tfile.write(source_video.read())
+                    vid_cap = cv2.VideoCapture(
+                        tfile.name)
+                    st_frame = st.empty()
+                    while (vid_cap.isOpened()):
+                        success, image = vid_cap.read()
+                        if success:
+                            _display_detected_frames(conf,
+                                                     model,
+                                                     st_frame,
+                                                     image
+                                                     )
+                        else:
+                            vid_cap.release()
+                            break
+                except Exception as e:
+                    st.error(f"Error loading video: {e}")
+
+
+def infer_uploaded_webcam(conf, model):
+    """
+    Execute inference for webcam.
+    :param conf: Confidence of YOLOv8 model
+    :param model: An instance of the `YOLOv8` class containing the YOLOv8 model.
+    :return: None
+    """
+    try:
+        flag = st.button(
+            label="Stop running"
+        )
+        vid_cap = cv2.VideoCapture(0)  # local camera
+        st_frame = st.empty()
+        while not flag:
+            success, image = vid_cap.read()
+            if success:
+                _display_detected_frames(
+                    conf,
+                    model,
+                    st_frame,
+                    image
+                )
+            else:
+                vid_cap.release()
+                break
+    except Exception as e:
+        st.error(f"Error loading video: {str(e)}")
+
+
+def show_chart(chart_no):
+    if chart_no == 1:
+        st.image('charts\F1_curve.png', 'F1 Curve')
+    elif chart_no == 2:
+        st.image('charts\R_curve.png', 'R Curve')
+    elif chart_no == 3:
+        st.image('charts\P_curve.png', 'P Curve')
+    elif chart_no == 4:
+        st.image('charts\confusion_matrix.png', 'Confuision Matrix')
+    elif chart_no == 5:
+        st.image('charts\PR_curve.png', 'PR Curve')
+    elif chart_no == 6:
+        st.image('charts\/results.png', 'Results')
